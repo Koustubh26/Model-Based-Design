e81e6d5f8d4d5bd1a5d59ac0129245e60e48ef27
Koustubh26 <31567538+Koustubh26@users.noreply.github.com>
2021-01-16 23:39:49 +0530

    Add files via upload

---
+++ MATLAB/Forward_Converter_Mathematical_Modelling/Forward_Converter_Script.m
@@ -0,0 +1,12 @@
+% Forward Converter Script
+
+D = 0.416; % Duty Cycle
+Fsw = 400e03; % Switching Frequency
+Vin = 12; % Input Voltage
+L = 12.12e-06; % Inductor value
+C = 19.5e-06; % Capacitor value
+R = 5; % Load Resistance
+Vd = 0.7; % Diode voltage drop
+Rd = 1e-3; % Diode resistance
+N2 = 4; % Secondary Side turns ratio
+N1 = 2; % Primary Side turns ratio
